7a4c7c2fae1808d8758834dab052c4c366373922
mukesh <kumarmukesh789123@gmail.com>
2024-02-01 18:34:14 +0500

    first commit

---
+++ dart.dart
@@ -0,0 +1,298 @@
+// dart
+// flutter (mobile application) cross platform / angular dart (web application)
+// vs code 
+// dart direct match C++
+
+
+
+// import 'dart:io';
+// void main(){
+//   var a = 100;
+//   int b =100;
+//   int c = 22;
+//   print(c);
+//   print(b);
+
+//  Float b = 78.89;  X
+// double b = 78.89;
+//  print(b);
+
+//   print("Hello World");
+//  print('Mobile Application with dart');
+
+// String name =" mr google coder";
+// print(name);
+// String name ='M';
+// print(name);
+
+// bool is_my_name
+// =true;
+// bool is_my_name1 =false;
+// print(is_my_name);
+// print(is_my_name1);
+
+// int a= 897;
+// int b= 100;
+// String name = 'Mr Google coder';
+// print('the value of a is: $a');
+// print('MY youtube channel name is : $name');
+
+
+// operators
+// arthematic operators + / * −  minus 
+// int a=8;
+// int b=9;
+
+// int add = a + b;
+// int multiply = a*b;
+// double divide= a/b;
+// int subs = a − b;
+// print("The subs of a and b is: $subs")
+
+// print("The sum of a and b is: $add");
+// print("The multiply of a and b is: $multiply");
+// print(' The division of a and b is: $divide');
+
+// double dividess= a/b;
+// print(' The division of a and b is: $dividess');
+
+
+// // user se input
+
+// print(" Enter your name ");
+// String name = stdin.readLineSync()!;
+// print('user name is: $name');
+
+// print("Enter your numbers");
+
+// int a = int.parse(stdin.readLineSync()!);
+// int b = int.parse(stdin.readLineSync()!);
+
+// int add = a + b;
+// print('the sum of a and b is: $add');
+
+// double a = double.parse(stdin.readLineSync()!);
+// double b = double.parse(stdin.readLineSync()!);
+
+// double multiply = a * b;
+// print('the multiplicatiion is: $multiply');
+
+// opertaors
+// int a =7;
+// a += 5;
+// a *= 2;
+
+// double b=8;
+// b==3;
+
+// print(b==3);
+// print(b==8);
+// comparison opertaors <, > <=, >=,  ==, 
+// assignment opertaors =, +=, *=,,,,
+// int c = 10;
+// int d =12;
+// // print(c>d);
+// // print(d >= c);
+// print(c=d);
+
+// logical operators && and , || or , ! not 
+// int a = 89;
+// int b = 89;
+// int c = 9;
+
+
+// print(a!=b);
+
+// print(a>b || b<c);
+// print(b>a || b<c);
+
+
+// print( a>c &&  b>c );
+// print(a>c &&  c>b);
+// print(a<c &&  c>b);
+
+// conditions statementns
+//  if and else , else if
+// print('enter your age');
+// int age = int.parse( stdin.readLineSync()!);
+
+// if (age > 18) {
+// print('you are eligible for ID card ');
+// }
+
+// else  {
+//    print("Your r not eligible for id card");
+// }
+
+// print('sallary for companies ');
+// int salary = int.parse( stdin.readLineSync()!);
+
+// if ( salary > 180000 && salary < 400000 ){
+//   print('you r google employee');
+// }
+
+// else if ( salary > 50000 && salary < 180000){
+//   print('working in amazon');
+
+// }
+
+// else if (salary>=50000 && salary<=100000){
+//   print('Balck rock ');
+// }
+
+// else if (salary>100000){
+//   print('billionaire');
+// }
+
+// else{
+//   print('Berozgar');
+// }
+
+// for(int i=1; i<=20; i++){
+//   print(i);
+// }
+
+
+// for ( int i=10; i>=1; i--){
+//   print(i);
+// }
+
+// int a=1;
+// while (a<11){
+//   print(a);
+//   a++;
+// }
+// int a=1;
+// do { 
+//   print(a);
+//   a++;
+
+// }  while(a<=10);
+
+// int a=100;
+// do { 
+//   print(a);
+//   a++;
+
+// }  while(a<10);
+// even numbers in loops  2 to 20
+// modulus operators  %
+// 8%2==0
+
+// for (int i=1; i<=20; i++){
+// if (i%2==0){
+//   print(i);
+// }
+// }
+
+// int i=2;
+// while(i<=20){
+//   if (i%2==0){
+//     print(i);
+//   }
+//   i++;
+// }
+
+
+// }
+
+
+
+// user define  builtin function
+
+// int sum_function(int a, int b) {
+//   return a+b;
+// }
+// void main(){
+// int a=5;
+// int b=8;
+// int result= sum_function(a,b);
+// print(result);
+// }
+
+// void printmessage(String stringmssge){
+//      print(stringmssge);
+// }
+
+// void main(){
+//    printmessage("dart");
+// }
+
+
+// list and map
+// void main(){
+ 
+//   List <String> fruits=['bannan', 'apple', 'grapes'];
+//    List <int> numbers=[34,89,123,456];
+//   print(numbers);
+//   print(numbers[2]);
+//   key and value pairs
+//    Map <String, int> studentsmarks={
+//       'joti':890,
+//       'kumar':1000,
+//       'vinod':200
+//    };
+
+//   //  print(studentsmarks);
+//   print(studentsmarks);
+
+// }
+
+
+
+// for each loop
+
+
+// void main(){
+//   List<int> numbers=[89,23,56,67,88,99];
+
+//   numbers.forEach((num) {
+//     print(num);
+
+//   });
+
+// }
+
+
+// class and object
+// class Person {
+//     String name;
+//     int age;
+
+//     Person(this.name , this.age);
+
+//     void sayHello() {
+       
+//        print('Hello, My name is $name and i am $age');
+
+//     }
+// }
+
+// void main() {
+//   var person = Person("Anjali" , 24);
+//   person.sayHello();
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
